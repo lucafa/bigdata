c0a40cee5651b5ef67c40e5c78ce0802a772dd0c
Luca Facchin <32875729+lucafa@users.noreply.github.com>
2018-10-17 00:25:19 +0200

    Add files via upload

--- classifier.py
+++ classifier.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 #Convert date in a readable format
-
+#****/
+#    /
+#    Write by Luca Facchin
+#                        /
+#                        /
+#                        /
+#TODO: Fix IndexError: list index out of range error
 from datetime import datetime
 import csv
 import pandas as  pd
@@ -8,60 +14,33 @@ import numpy as np
 from time import time
 import sys
 import os
-
-
 import csv
+
+#Variable declaration
+x=1
 year = "2018"
-with open('alert_simple.csv', 'r') as f, open('alert_edited2.csv', 'a') as f_out:
+
+
+# Main Program
+#Open file in read & write mode
+with open('alert_simple.csv', 'r+') as f, open('alert_edited2.csv', 'a') as f_out:
     reader = csv.DictReader(f)
     writer = csv.DictWriter(f_out, fieldnames=reader.fieldnames)
     writer.writeheader()  # For writing header
-    for row in reader:
-        if row['timestamp'] != '0':
-            data=row['timestamp']
-            data = data[:-1]
-            data = year + "/" + data
-            #print(data)
-            a=datetime.strptime(data , '%Y/%m/%d-%H:%M:%S.%f')
-            b = datetime(1970, 1, 1)
-            microseconds_date = ((a-b).total_seconds())
-            #print(microseconds_date)
-            data_ok = str(microseconds_date)
-            writer.writerow(data_ok)
+    data = [r for r in reader]
+#While to edit row by row into the csv file
+    while data:
+        tempo = data[x]['timestamp']
+        tempo = year+tempo
+        #remove last charachter of the timestamp if it's a space character 
+        if tempo.endswith(' '):
+            tempo=tempo[:-1]
+        #copy seconds value on new variable
+        new_tempo = (((datetime.strptime(tempo,"%Y%m/%d-%H:%M:%S.%f")-datetime(1970,1,1)).total_seconds()))
+        data[x]['timestamp'] = new_tempo
+        #Write values on new file
+        writer.writerow(data[x])
+        x += 1
+
             
 exit()
-
-
-#Read csv file to convert 
-data = pd.read_csv("alert2.csv")
-#print(data['timestamp'])
-#print((datetime.strptime((data['timestamp']),"%m/%d-%H:%M:%S.%f")-datetime(1970,1,1)).total_seconds())
-#data['Timeframe'] = (data['timestamp'])
-print(data['timestamp'].dt.hour)
-exit()
-#a=datetime.strptime(data2 , "%m/%d-%H:%M:%S.%f")
-b = datetime(1970, 1, 1)
-print(data2, "is type of: ", type(data2))
-print(b, "is type of: ", type(b))
-
-#print(-((a-b).total_seconds()))
-#print(data2.Timestamp())
-exit()
-
-
-for index, row in data.itertuples():
-    #print row["timestamp"], row["src"]
-    edit_time=row['timestamp']
-    print(edit_time())
-    #a = datetime.strptime((row['timestamp']) , "%m/%d-%H:%M:%S.%f")
-    #b = datetime(1970, 1, 1)
-    #print(-((a-b).total_seconds()))
-
-
-#data['timestamp'] = ((date.strptime(data['timestamp'],"%m/%d-%H:%M:%S.%f")-datetime(1970,1,1)).total_seconds())
-exit()
-a = datetime.strptime('10/06-15:39:39.513121' , "%m/%d-%H:%M:%S.%f")
-b = datetime(1970, 1, 1)
-print(-((a-b).total_seconds()))
-
-
